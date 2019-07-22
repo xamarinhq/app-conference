a85bcab332fc929d9605c0bde81e8e902b9bd406
luismts <deivi_0326@hotmail.com>
2019-07-22 19:21:39 -0400

    Default data feed added

--- src/Conference.Backend/Models/ConferenceContextInitializer.cs
+++ src/Conference.Backend/Models/ConferenceContextInitializer.cs
@@ -8,7 +8,29 @@ namespace Conference.Backend.Models
     {
         protected override void Seed(ConferenceContext context)
         {
-            //Seed Data Here
+            try
+            {
+                DefaultData.DefaultDataSetup.AddAll(context);
+                context.SaveChanges();
+            }
+            catch (System.Data.Entity.Validation.DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        System.Diagnostics.Trace.TraceInformation(
+                              "Class: {0}, Property: {1}, Error: {2}",
+                              validationErrors.Entry.Entity.GetType().FullName,
+                              validationError.PropertyName,
+                              validationError.ErrorMessage);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.TraceError(ex.Message + "\n" + ex.InnerException.Message + "\n" + ex.StackTrace);
+            }
         }
     }
 }
