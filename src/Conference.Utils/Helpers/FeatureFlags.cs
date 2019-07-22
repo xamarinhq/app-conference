e3a47bd205aac41b3f35c32658873658097e5f16
luismts <deivi_0326@hotmail.com>
2019-07-22 19:29:48 -0400

    Some feature added

--- src/Conference.Utils/Helpers/FeatureFlags.cs
+++ src/Conference.Utils/Helpers/FeatureFlags.cs
@@ -6,7 +6,7 @@ namespace Conference.Utils.Helpers
 {
     public static class FeatureFlags
     {
-        public static bool LoginEnabled => false; // Set to true to use original login provider; false means that anonymous accounts are used
+        public static bool LoginEnabled => true; // Set to true to use original login provider; false means that anonymous accounts are used
         public static bool MiniHacksEnabled => false;
         public static bool EventsEnabled => true;
         public static bool ShowBuyTicketButton => false;
@@ -24,7 +24,19 @@ namespace Conference.Utils.Helpers
         public static bool AppToWebLinkingEnabled => true;
         public static bool ShowLocationInSessionCell => true;
 
-        public static bool UseMocks => true;
+        /// <summary>
+        /// Out of the box the Conference Mobile app uses sample data provided by the <see cref="Conference.DataStore.Mock"/>. 
+        /// This is great for development, but you can also test against the test/development read-only Azure App Server Mobile Apps backend
+        /// </summary>
+        /// <remarks>
+        /// // Simply change:
+        /// public static bool UseMocks => true;
+        /// 
+        /// // to:
+        /// public static bool UseMocks => false;
+        /// </remarks>
+        public static bool UseMocks => false;
+
         public static bool HockeyAppEnabled => false;
         public static bool GoogleAnalyticsEnabled => false;
     }
